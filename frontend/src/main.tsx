736aa28f3e78929e8127ad4bd3967ef57e1a7c7a
homuran76 <nami76.homuran@gmail.com>
2025-11-02 08:15:10 +0900

    不要なコメント、不要なコンポーネントの削除

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,10 +6,6 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/*
-      TODO: Replace with your actual Google Client ID
-      You can get it from the Google Cloud Console: https://console.cloud.google.com/apis/credentials
-    */}
     <GoogleOAuthProvider clientId="143242869279-ld2vc4b1sp8pll96h2i5reqv311nf3h8.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
